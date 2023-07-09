c16ad6e04dd176f9bb140064162901c3e78157f7
Yarumis Rodriguez <107522978+YarumisGinanRM@users.noreply.github.com>
2023-07-09 05:51:53 +0000

    funcionalidad y diseño completado

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -3,15 +3,30 @@ import "../../styles/trafficLight.css"
 
 
 const TrafficLight = () => {
-    const [ colorActivo, setColorActivo] = useState("verde")
+    const [colorActivo, setColorActivo] = useState("verde")
+
+    const cambiarColor = () => {
+        if (colorActivo === "verde") setColorActivo("amarillo")
+        if (colorActivo === "rojo") setColorActivo("verde")
+        if (colorActivo === "amarillo") setColorActivo("rojo")
+    }
 
     return (
         <>
-            <div className="semaforo ">
-               <div className="red"></div>
-               <div className="amarillo"></div>
-               <div className={`verde ${colorActivo == "verde" ? "brillo" : " "}`}></div>
+            <div>
+                <div className="semaforo mt-5 p-1 bg-dark flex-column">
+                    <div onClick={() => setColorActivo("rojo")} className={`rojo ${colorActivo === "rojo" ? "brillo" : ""}`}>
+                    </div>
+                    <div onClick={() => setColorActivo("verde")} className={`amarillo ${colorActivo === "amarillo" ? "brillo" : ""}`}>
+                    </div>
+                    <div onClick={() => setColorActivo("amarillo")} className={`verde ${colorActivo === "verde" ? "brillo" : ""}`}>
+                    </div>
+                </div>
+                <div className="button">
+                <button type="button" onClick={cambiarColor} class="btn btn-light p-3 m-5 fs-5" style={{width: "250px"}}>Cambiar Color</button>
+                </div>
             </div>
+
         </>
     )
 }
